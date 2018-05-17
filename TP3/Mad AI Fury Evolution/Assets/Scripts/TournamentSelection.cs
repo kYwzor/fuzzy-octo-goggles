cca94ddeb056618ca14e1749c43c4881559d83cb
kYwzor <tiago_gomes_76@hotmail.com>
2018-05-18 00:08:55 +0100

    BABUINOS

--- TP3/Mad AI Fury Evolution/Assets/Scripts/TournamentSelection.cs
+++ TP3/Mad AI Fury Evolution/Assets/Scripts/TournamentSelection.cs
@@ -3,37 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TournamentSelection : SelectionMethod{
-	public float k = 1;
     public override List<Individual> selectIndividuals(List<Individual> oldpop, int tournamentSize){
-		float[] probs = new float[tournamentSize];
 		List<Individual> newPop = new List<Individual>();
-		for(int i = 0; i < tournamentSize; i++){
-			probs[i] = k * 	Mathf.Pow((1 - k), i);
-			if(i != 0) probs[i] += probs[i - 1];
-			//Debug.Log("Prob " + i + " " + probs[i]);
-		}
 		for(int i = 0; i < oldpop.Count; i++){
-			List<Individual> candidates = new List<Individual>();
-			for(int j = 0; j < tournamentSize; j++){
-				candidates.Add(oldpop[Random.Range(0,oldpop.Count)]);
+			Individual bestCandidate = oldpop[Random.Range(0,oldpop.Count)];
+			for(int j = 1; j < tournamentSize; j++){
+				Individual currentCandidate = oldpop[Random.Range(0,oldpop.Count)];
+				if(currentCandidate.Fitness > bestCandidate.Fitness){
+					bestCandidate = currentCandidate;
+				}
 			}
-			candidates.Sort(delegate(Individual x, Individual y){
-				if(x.Fitness > y.Fitness){
-					return -1;
-				}
-				else if(y.Fitness > x.Fitness){
-					return 1;
-				}
-				else{
-					return 0;
-				}
-			});
-			int h = 0;
-			float r = Random.value;
-			while(r > probs[h] && h < tournamentSize - 1){
-				h++;
-			}
-			newPop.Add(candidates[h]);
+			newPop.Add(bestCandidate);
 		}
 		return newPop;
     }
